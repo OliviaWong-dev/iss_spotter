9101d99459d6aac8c3525aedcaef2bfb12b96516
Olivia Wong <olivia.wy.wong@gmail.com>
2021-08-21 03:23:46 +0000

    ISS SPotter |||

--- iss.js
+++ iss.js
@@ -8,44 +8,61 @@
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
-// const fetchMyIP = function (callback) {
-//   request("https://api.ipify.org?format=json", (error, response, body) => {
-//     if (error) return callback(error, null);
 
+const request = require("request");
+
+// const fetchCoordsByIP = function (ip, callback) {
+//   request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+//     if (error) {
+//       callback(error, null);
+//       return;
+//     }
 //     if (response.statusCode !== 200) {
 //       callback(
-//         Error(`Status Code ${response.statusCode} when fetching IP: ${body}`),
+//         Error(
+//           `Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`
+//         ),
 //         null
 //       );
 //       return;
 //     }
+//     const { passes } = JSON.parse(body).response;
 
-//     const ip = JSON.parse(body).ip;
-//     callback(null, ip);
+//     callback(null, passes);
 //   });
 // };
 
-const request = require("request");
-
-const fetchCoordsByIP = function (ip, callback) {
-  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
-    if (error) {
-      callback(error, null);
-      return;
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+const fetchISSFlyOverTimes = function (coords, callback) {
+  request(
+    `http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`,
+    (error, response, body) => {
+      if (error) {
+        callback(error, null);
+        return;
+      }
+      if (response.statusCode !== 200) {
+        callback(
+          Error(
+            `Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`
+          ),
+          null
+        );
+        return;
+      }
+      const passes = JSON.parse(body).response;
+      callback(null, passes);
     }
-    if (response.statusCode !== 200) {
-      callback(
-        Error(
-          `Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`
-        ),
-        null
-      );
-      return;
-    }
-    const { latitude, longitude } = JSON.parse(body);
-
-    callback(null, { latitude, longitude });
-  });
+  );
 };
 
-module.exports = { fetchCoordsByIP };
+module.exports = { fetchISSFlyOverTimes };
