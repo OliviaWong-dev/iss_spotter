93c4f43f32df33cbf9f985d32706153eadd9a4ab
Olivia Wong <olivia.wy.wong@gmail.com>
2021-08-21 18:42:59 +0000

    ISS Spotter with promises

---
+++ index2.js
@@ -0,0 +1,18 @@
+const { nextISSTimesForMyLocation } = require("./iss_promised");
+
+const printPassTimes = function (passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+// see index.js for printPassTimes
+// copy it from there, or better yet, moduralize and require it in both files
+
+// Call
+nextISSTimesForMyLocation().then((passTimes) => {
+  printPassTimes(passTimes);
+});
